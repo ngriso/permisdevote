09e82d2a5140f3977ae9b1d9a11686abcd027500
Nicolas Griso <nicolas.griso@gmail.com>
2012-03-17 16:11:13 +0100

    make fetch the elections work

--- src/main/java/hte/Fetcher.java
+++ src/main/java/hte/Fetcher.java
@@ -7,21 +7,23 @@ import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("fetch")
+@Produces(MediaType.APPLICATION_JSON)
 public class Fetcher {
 
     @GET
     @Path("2012")
-    public Election fetchElection(){
+    public Election fetchElection() {
         String idElection2012 = "4f16fe2299c7a10001000012";
         String election2012URL = "http://voxe.org/api/v1/elections/4f16fe2299c7a10001000012";
 
         ClientConfig cc = new DefaultClientConfig();
+        cc.getClasses().add(ContextResolverForObjectMapper.class);
         cc.getClasses().add(JacksonJsonProvider.class);
         cc.getFeatures().put("com.sun.jersey.api.json.POJOMappingFeature", true);
-        return Client.create(cc).resource(election2012URL).get(ResponseElection.class).response;
-
-        
+        return Client.create(cc).resource(election2012URL).accept(MediaType.APPLICATION_JSON_TYPE).get(ResponseElection.class).response.election;
     }
 }
