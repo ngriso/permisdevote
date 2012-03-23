b4fe998a665af233316eb78174ee91a367348034
Nicolas Griso <nicolas.griso@gmail.com>
2012-03-23 17:16:56 +0100

    work

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,126 +1,147 @@
-var start = function() {
-    p2v = {};
-    p2v.model = {};
-    p2v.username = "nicolas";
-    p2v.urls = {};
-    p2v.urls.themes = "./api/themes";
-    p2v.urls.candidacies = "./api/candidacies";
-    p2v.urls.voters = "./api/voters";
-    p2v.urls.questions = "./api/questions";
-    p2v.urls.questions_next = p2v.urls.questions + "/next?{:type}={:typeId}";
-    p2v.urls.answer = p2v.urls.questions + "/{:questionId}/answer?answer={:answer}&username={:username}";
-
-    $.when(
-            $.get(p2v.urls.candidacies, function(data) {
-                p2v.model.candidacies = data;
-            }),
-            $.get(p2v.urls.themes, function(data) {
-                p2v.model.themes = data;
-            })
-    ).done(function() {
-                var dirForCandidacies = {
-                    'div' :{
-                        'candidacy <- candidacies' :{
-                            '.': "#{candidacy.candidate1.firstName}  #{candidacy.candidate1.lastName}",
-                            '.@data-id': "candidacy.id",
-                            '.@data-type': function() {
-                                return "candidacyId";
-                            },
-                            '.@data-info': "#{candidacy.candidate1.firstName}  #{candidacy.candidate1.lastName}"
-                        }
-                    }
-                };
-                var dirForThemes = {
-                    'div' :{
-                        'theme <- themes' :{
-                            '.': "theme.name",
-                            '.@data-id': "theme.id",
-                            '.@data-type': function() {
-                                return "tagId";
-                            },
-                            '.@data-info': "theme.name"
-                        }
-                    }
-                };
-
-                $("#selection div.candidacies").render(p2v.model, dirForCandidacies);
-                $("#selection div.themes").render(p2v.model, dirForThemes);
-                $("#selection div div.selectionItem").click(clickOnCandidacyOrTheme);
-                $("img", "#compte").click(function() {
-                    var profile = {username: $("#username").val()};
-                    $.ajax({
-                        type:'POST',
-                        url:p2v.urls.voters,
-                        data:JSON.stringify(profile),
-                        contentType:"application/json"
-                    });
-                });
-            });
-
-    $("#nextQuestionBtn").click(nextQuestion);
-};
-
-var clickOnCandidacyOrTheme = function (event) {
-    var type = $(event.currentTarget).attr("data-type");
-    var id = $(event.currentTarget).attr("data-id");
-    p2v.currentInfo = $(event.currentTarget).attr("data-info");
-    console.log(formatString("Selected badge: type:[{:type}], id:[{:id}]", {type:type, id:id}));
-    var questionUrl = formatString(p2v.urls.questions_next, {type:type, typeId:id});
-    nextQuestion(questionUrl, type);
-    var regex = /(.*)[#]?/;
-    var match = regex.exec(window.location.href);
-    window.location.href = match[1] + "#question";
-};
-
-var nextQuestion = function(questionUrl, type) {
-    $.get(questionUrl, function(data) {
-        if (type === 'tagId') {
-            $(".questionCandidacyText").hide();
-            var dirForQuestionTheme = {
-                'input@data-questionId':"question.id",
-                'span[data-selector="currentInfo"]':"currentInfo",
-                'span[data-selector="candidate"]':"#{question.candidacy.candidate1.firstName} #{question.candidacy.candidate1.lastName}",
-                'span[data-selector="question"]':"question.text"
-            };
-            $(".questionThemeText").render({question:data, currentInfo: p2v.currentInfo}, dirForQuestionTheme);
-            $(".questionThemeText").show();
-        } else {
-            $(".questionThemeText").hide();
-            var dirForQuestionCandidacy = {
-                'input@data-questionId':"question.id",
-                'span[data-selector="currentInfo"]':"currentInfo",
-                'span[data-selector="theme"]':"question.tagLevel1.name",
-                'span[data-selector="question"]':"question.text"
-            };
-            $(".questionCandidacyText").render({question:data, currentInfo: p2v.currentInfo}, dirForQuestionCandidacy);
-            $(".questionCandidacyText").show();
+var application = {
+    initialize:function() {
+        application.model = {};
+        application.data = {
+            userID : "",
+            currentType : "",
+            currentSelectedTypeId:""
+        };
+        application.urls = {
+            themes : "./api/themes",
+            candidacies : "./api/candidacies",
+            voters : "./api/voters",
+            voter_stats : function() {
+                return "./api/voters/" + application.data.userID + "/stats"
+            },
+            next_question: function() {
+                return "./api/questions/next?" + (application.data.currentType === 'tagId' ? "tagId=" : "candidacyId=") + application.data.currentSelectedTypeId
+            },
+            answer : function(questionID, answer) {
+                return "./api/questions/" + questionID + "/answer?answer=" + answer + "&userID=" + application.data.userID
+            }
         }
-
-        $(":radio").click(function(event) {
-            var value = $(event.currentTarget).val();
-            var questionId = $(".questionText :hidden").attr("data-questionId");
-            p2v.username = $("#username").val();
-            var answerURL = formatString(p2v.urls.answer, {questionId:questionId, answer:value, username:p2v.username});
-            console.log(answerURL);
-            $.get(answerURL, function(data) {
-                console.log(data);
-                p2v.username = $("#username").val();
-
-                $.get("/api/stats/" + p2v.username, function(data) {
-                    $(".badges").html($("#badgesTmpl").tmpl(data));
-                    nextQuestion();
+    },
+    start : function() {
+        application.initialize();
+        $.when(
+                $.get(application.urls.candidacies, function(data) {
+                    application.model.candidacies = data;
+                }),
+                $.get(application.urls.themes, function(data) {
+                    application.model.themes = data;
+                })
+        ).done(function() {
+                    $("#compte img").click(application.clickOnStartPermis);
+                    $(":radio").click(application.clickOnResponse);
                 });
+    },
+    clickOnStartPermis : function() {
+        var profile = {username: $("#username").val()};
+        $.ajax({
+            type:'POST',
+            url:application.urls.voters,
+            data:JSON.stringify(profile),
+            contentType:"application/json",
+            success:function(data) {
+                application.data.userID = data.id;
+            }
+        });
+
+        $("#selection").show();
+        application.renderCandidaciesList();
+        application.renderThemeList();
+    },
+    renderCandidaciesList:function() {
+        var dirForCandidacies = {
+            'div' :{
+                'candidacy <- candidacies' :{
+                    '.': "#{candidacy.candidate1.firstName}  #{candidacy.candidate1.lastName}",
+                    '.@data-id': "candidacy.id",
+                    '.@data-info': "#{candidacy.candidate1.firstName}  #{candidacy.candidate1.lastName}"
+                }
+            }
+        };
+        $("#selection div.candidacies").render(application.model, dirForCandidacies);
+        $("div.selectionItem.candidacy").click(function(event) {
+            application.data.currentType = "candidacyId";
+            application.data.currentSelectedTypeId = $(event.currentTarget).attr("data-id");
+            application.currentInfo = $(event.currentTarget).attr("data-info");
+            application.nextQuestion();
+        });
+    },
+    renderThemeList:function() {
+        var dirForThemes = {
+            'div' :{
+                'theme <- themes' :{
+                    '.': "theme.name",
+                    '.@data-id': "theme.id",
+                    '.@data-info': "theme.name"
+                }
+            }
+        };
+        $("#selection div.themes").render(application.model, dirForThemes);
+        $(".selectionItem.theme").click(function(event) {
+            application.data.currentType = "tagId";
+            application.data.currentSelectedTypeId = $(event.currentTarget).attr("data-id");
+            application.currentInfo = $(event.currentTarget).attr("data-info");
+            application.nextQuestion();
+        });
+    },
+    nextQuestion : function() {
+        $.get(application.urls.next_question(), function(data) {
+            $("div.question").attr("data-questionId", data.id);
+            $(".questionThemeText").hide();
+            $(".questionCandidacyText").hide();
+            if (application.data.currentType === 'tagId') {
+                application.renderQuestionTheme(data);
+            } else {
+                application.renderQuestionCandidacy(data);
+            }
+            $(".answers").show();
+            $(".gotoNextQuestion").hide();
+            $(".answers :radio").each(function() {
+                $(this).prop('checked', false)
             });
         });
-    });
+    },
+    renderQuestionTheme:function(data) {
+        var dirForQuestionTheme = {
+            'span[data-selector="currentInfo"]':"currentInfo",
+            'span[data-selector="candidate"]':"#{question.candidacy.candidate1.firstName} #{question.candidacy.candidate1.lastName}",
+            'span[data-selector="question"]':"question.text"
+        };
+        $(".questionThemeText").render({question:data, currentInfo: application.currentInfo}, dirForQuestionTheme);
+        $(".questionThemeText").show();
+    },
+    renderQuestionCandidacy:function(data) {
+        var dirForQuestionCandidacy = {
+            'span[data-selector="currentInfo"]':"currentInfo",
+            'span[data-selector="theme"]':"question.tagLevel1.name",
+            'span[data-selector="question"]':"question.text"
+        };
+        $(".questionCandidacyText").render({question:data, currentInfo: application.currentInfo}, dirForQuestionCandidacy);
+        $(".questionCandidacyText").show();
+    },
+    clickOnResponse : function(event) {
+        var value = $(event.currentTarget).val();
+        var questionId = $("div.question").attr("data-questionId");
+        var answerURL = application.urls.answer(questionId, value);
+        $.get(answerURL, function(data) {
+            if (data) {
+                $("h2.responseTextIncorrect").hide();
+                $("h2.responseTextCorrect").show();
+            } else {
+                $("h2.responseTextCorrect").hide();
+                $("h2.responseTextIncorrect").show();
+            }
+            $("div.question :button").click(application.nextQuestion);
+            var voterStatsURL = application.urls.voter_stats();
+            $.get(voterStatsURL, function(data) {
+                $(".badges").html($("#badgesTmpl").tmpl(data));
+                $(".answers").hide();
+                $(".gotoNextQuestion").show();
+            });
+        });
+    }
 };
 
-var formatString = function(string, params) {
-    for (var key in params) {
-        if (params.hasOwnProperty(key)) {
-            var value = params[key];
-            string = string.replace("{:" + key + "}", value);
-        }
-    }
-    return string;
-};
